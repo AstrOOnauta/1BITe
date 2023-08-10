b5cdb7fdb324879104688cd7ccdca3ebe5d6a367
Will <willian.ralf.rn@gmail.com>
2023-08-10 00:03:45 -0300

    feat: dashboard header layout

--- src/components/ProfileMenu/index.tsx
+++ src/components/ProfileMenu/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable sonarjs/no-duplicate-string */
+/* eslint-disable react/jsx-no-bind */
 import {
   Avatar,
   Button,
@@ -7,16 +9,30 @@ import {
   MenuItem,
   MenuList,
   Text,
+  theme,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { MdExitToApp, MdExpandMore, MdHome, MdPerson } from "react-icons/md";
 
 export default function ProfileMenu() {
+  const router = useRouter();
+
+  const isMobileVersion = useBreakpointValue({
+    base: true,
+    md: false,
+  });
+
+  function logout() {
+    router.push("/");
+  }
+
   return (
     <Menu>
       <MenuButton
         as={Button}
-        rightIcon={<MdExpandMore size={24} />}
+        rightIcon={isMobileVersion ? undefined : <MdExpandMore size={24} />}
         variant="solid"
         backgroundColor="blue.900"
         color="green.50"
@@ -26,29 +42,65 @@ export default function ProfileMenu() {
         _active={{
           backgroundColor: "blue.900",
         }}
+        borderRadius={isMobileVersion ? "full" : 6}
+        px={isMobileVersion ? 1 : 4}
       >
         <Flex alignItems="center" gap={4}>
           <Avatar size="sm" />
-          <Text>Usuário</Text>
+          {isMobileVersion ? null : <Text>Usuário</Text>}
         </Flex>
       </MenuButton>
       <MenuList bg="green.50" shadow="xl">
-        <MenuItem as={Link} href="#" bg="green.50">
+        <MenuItem
+          as={Link}
+          href="/meus-restaurantes"
+          bg={router.asPath === "/meus-restaurantes" ? "blue.900" : "green.50"}
+        >
           <Flex w="100%" justifyContent="space-between">
-            <Text color="blue.900">Perfil</Text>
-            <MdPerson size={24} />
+            <Text
+              color={
+                router.asPath === "/meus-restaurantes" ? "green.50" : "blue.900"
+              }
+            >
+              Perfil
+            </Text>
+            <MdPerson
+              size={24}
+              color={
+                router.asPath === "/meus-restaurantes"
+                  ? theme.colors.green[50]
+                  : theme.colors.blue[900]
+              }
+            />
           </Flex>
         </MenuItem>
-        <MenuItem as={Link} href="#" bg="green.50">
+        <MenuItem
+          as={Link}
+          href="/meus-restaurantes"
+          bg={router.asPath === "/meus-restaurantes" ? "blue.900" : "green.50"}
+        >
           <Flex w="100%" justifyContent="space-between">
-            <Text color="blue.900">Meus Restaurantes</Text>
-            <MdHome size={24} />
+            <Text
+              color={
+                router.asPath === "/meus-restaurantes" ? "green.50" : "blue.900"
+              }
+            >
+              Meus Restaurantes
+            </Text>
+            <MdHome
+              size={24}
+              color={
+                router.asPath === "/meus-restaurantes"
+                  ? theme.colors.green[50]
+                  : theme.colors.blue[900]
+              }
+            />
           </Flex>
         </MenuItem>
-        <MenuItem as="button" onClick={() => null} bg="green.50">
+        <MenuItem as="button" onClick={logout} bg="green.50">
           <Flex w="100%" justifyContent="space-between">
             <Text color="blue.900">Sair</Text>
-            <MdExitToApp size={24} />
+            <MdExitToApp size={24} color={theme.colors.blue[900]} />
           </Flex>
         </MenuItem>
       </MenuList>
--- src/routes/private.routes.tsx
+++ src/routes/private.routes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Flex, Stack, useBreakpointValue } from "@chakra-ui/react";
 
 import SideBar from "~/components/SideBar";
+import DashboardHeader from "~/components/DashboardHeader";
 import { ChildrenInterface } from "~/shared/interfaces/general/childrenNode";
 
 const PrivateRoutes: React.FC<ChildrenInterface> = ({ children }) => {
@@ -14,7 +15,10 @@ const PrivateRoutes: React.FC<ChildrenInterface> = ({ children }) => {
   return (
     <Flex minH="100vh">
       {isTabletVersion ? null : <SideBar />}
-      <Stack w="100%">{children}</Stack>
+      <Stack w="100%">
+        <DashboardHeader />
+        {children}
+      </Stack>
     </Flex>
   );
 };
