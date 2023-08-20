910db44c0f0f3428cba41c313e2e51f795fd896f
Will <willian.ralf.rn@gmail.com>
2023-08-20 09:25:12 -0300

    fix: removed getCities fetch - dashboard settings page

--- src/pages/[restaurantId]/dashboard/configuracoes/index.page.tsx
+++ src/pages/[restaurantId]/dashboard/configuracoes/index.page.tsx
@@ -1,14 +1,7 @@
 import type { NextPage } from "next";
 import { Flex, Text } from "@chakra-ui/react";
-import { useEffect } from "react";
-
-import { getCities } from "~/shared/services/api/brasilAberto";
 
 const DashboardSettings: NextPage = () => {
-  useEffect(() => {
-    getCities("RN").then((res) => console.log(res));
-  }, []);
-
   return (
     <Flex flex={1} alignItems="center" justifyContent="center" overflowY="auto">
       <Text fontSize="2xl" fontWeight="bold">
